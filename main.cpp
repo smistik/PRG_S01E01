6ca769d551c5b1a9f0e49ab6a04c5c49cd0ea800
smistik <smistik@stredniskola.com>
2022-10-18 15:53:39 +0200

    Přidáno načtení vstupu a výpis výpočtu

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main(){
-    cout << "Ahoj svete.";
+    cout << "Nazev ulohy: Vypocet prumerne denni teploty\n" << endl;
+    float t1;  //teplota v 6 hodin ráno
+    float t2;  //teplota v 12 hodin
+    float t3;  //teplota v 18 hodin
+    cout << "Zadej teplotu v 6,00 hodin: ";
+    cin >> t1;
+    cout << "Zadej teplotu ve 12,00 hodin: ";
+    cin >> t2;
+    cout << "Zadej teplotu v 18,00 hodin: ";
+    cin >> t3;
+    cout << "Prumerna teplota je " << t1+t2+t3+t3/4 << " stupnu.\n";
     return 0;
 }
